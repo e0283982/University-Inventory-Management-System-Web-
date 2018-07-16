ff6ef676d69cd1289b257d5325a150cb92470b69
Chirag Shetty <e0283982@u.nus.edu>
2018-07-16 12:34:07 +0530

    Models and DAL
    Added Models and created Data Access Layer

---
+++ SA46Team1_Web_ADProj/DAL/CategoryRepositoryImpl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using SA46Team1_Web_ADProj.Models;
+
+namespace SA46Team1_Web_ADProj.DAL
+{
+    public class CategoryRepositoryImpl : ICategoryRepository, IDisposable
+    {
+        private SSISdbEntities context;
+        public CategoryRepositoryImpl(SSISdbEntities context)
+        {
+            this.context = context;
+        }
+        public void DeleteCategory(int categoryId)
+        {
+            Category category = context.Categories.Find(categoryId);
+            context.Categories.Remove(category);
+        }
+        public Category GetCategoryById(int categoryId)
+        {
+            return context.Categories.Find(categoryId);
+        }
+        public IEnumerable<Category> GetItems()
+        {
+            return context.Categories.ToList();
+        }
+        public void InsertCategory(Category category)
+        {
+            context.Categories.Add(category);
+        }
+        public void UpdateCategory(Category category)
+        {
+            context.Entry(category).State = EntityState.Modified;
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
