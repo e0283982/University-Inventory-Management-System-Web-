e75ad3076b572d11b70f12cba757f3c619cfc430
SandyIsHere <sandyongsowt@gmail.com>
2018-07-17 22:20:09 +0800

    added Restful Controller for EF -> JSON objs

---
+++ SA46Team1_Web_ADProj/Controllers/RestfulController.cs
@@ -0,0 +1,27 @@
+﻿using SA46Team1_Web_ADProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+
+namespace SA46Team1_Web_ADProj.Controllers
+{
+    public class RestfulController : ApiController
+    {
+        [System.Web.Mvc.HttpPost]
+        [System.Web.Mvc.Route("api/Restful/EmployeeList")]
+        public List<Employee> GetEmployeesList()
+        {
+            using (SSISdbEntities m = new SSISdbEntities())
+            {
+                m.Configuration.ProxyCreationEnabled = false;
+                return m.Employees.ToList<Employee>();
+            }
+
+        }
+    }
+}
