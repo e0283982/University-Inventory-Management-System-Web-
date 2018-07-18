9ef3cf6a4b8bf5b4fd8f662e91592ff9c98eb398
Jayden <wwj.jayden@gmail.com>
2018-07-18 11:39:59 +0800

    fixed restfulcontroller for multiple methods

--- SA46Team1_Web_ADProj/App_Start/WebApiConfig.cs
+++ SA46Team1_Web_ADProj/App_Start/WebApiConfig.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace SA46Team1_Web_ADProj.App_Start
 {
@@ -13,11 +15,16 @@ namespace SA46Team1_Web_ADProj.App_Start
             config.Formatters.Remove(config.Formatters.XmlFormatter);
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                    defaults: new { id = RouteParameter.Optional }
-            );
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{action}/{id}",
+            //        defaults: new { id = RouteParameter.Optional }
+            //);
+
+            config.Routes.MapHttpRoute("DefaultApiWithId", "Api/{controller}/{id}", new { id = RouteParameter.Optional }, new { id = @"\d+" });
+            config.Routes.MapHttpRoute("DefaultApiWithAction", "Api/{controller}/{action}");
+            config.Routes.MapHttpRoute("DefaultApiGet", "Api/{controller}", new { action = "Get" }, new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) });
+            config.Routes.MapHttpRoute("DefaultApiPost", "Api/{controller}", new { action = "Post" }, new { httpMethod = new HttpMethodConstraint(HttpMethod.Post) });
         }
     }
 }
--- SA46Team1_Web_ADProj/Controllers/RestfulController.cs
+++ SA46Team1_Web_ADProj/Controllers/RestfulController.cs
@@ -10,18 +10,50 @@ using System.Data.Entity;
 
 namespace SA46Team1_Web_ADProj.Controllers
 {
+    [System.Web.Mvc.RoutePrefix("api/Restful")]
     public class RestfulController : ApiController
     {
-        [System.Web.Mvc.HttpPost]
-        [System.Web.Mvc.Route("api/Restful/EmployeeList")]
-        public List<Employee> GetEmployeesList()
+        [System.Web.Mvc.HttpGet]
+        [System.Web.Mvc.Route("GetEmployeeList")]
+        public List<Employee> GetEmployeeList()
         {
             using (SSISdbEntities m = new SSISdbEntities())
             {
                 m.Configuration.ProxyCreationEnabled = false;
                 return m.Employees.ToList<Employee>();
             }
+        }
 
+        [System.Web.Mvc.HttpGet]
+        [System.Web.Mvc.Route("GetItemCodeList")]
+        public List<String> GetItemCodeList()
+        {
+            using (SSISdbEntities m = new SSISdbEntities())
+            {
+                return m.InventoryOverviews.Select(x => x.ItemCode).ToList<String>();
+            }
+        }
+
+        [System.Web.Mvc.HttpGet]
+        [System.Web.Mvc.Route("GetInventoryOverviewList/{itemcode}")]
+        public List<InventoryOverview> GetInventoryOverviewList(string itemcode)
+        {
+            using (SSISdbEntities m = new SSISdbEntities())
+            {
+                m.Configuration.ProxyCreationEnabled = false;
+                return m.InventoryOverviews.Where(x => x.ItemCode == itemcode).ToList<InventoryOverview>();
+            }
+        }
+
+        [System.Web.Mvc.HttpGet]
+        [System.Web.Mvc.Route("GetGoodsReceivedLists/{itemcode}")]
+        public List<GoodsReceivedList> GetGoodsReceivedLists(string itemcode)
+        {
+            using (SSISdbEntities m = new SSISdbEntities())
+            {
+                m.Configuration.ProxyCreationEnabled = false;
+                return m.GoodsReceivedLists.Where(x => x.ItemCode == itemcode).ToList<GoodsReceivedList>();
+            }
         }
     }
 }
