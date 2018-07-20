176822aa814f4272becc2bb4caf7dcc52bb6fa9c
SandyIsHere <sandyongsowt@gmail.com>
2018-07-20 13:31:52 +0800

    updated: DEPT, Authorisation and Requisition pages

--- SA46Team1_Web_ADProj/Controllers/DeptRequisitionController.cs
+++ SA46Team1_Web_ADProj/Controllers/DeptRequisitionController.cs
@@ -11,10 +11,30 @@ namespace SA46Team1_Web_ADProj.Controllers
     [RoutePrefix("Dept/DeptRequisition")]
     public class DeptRequisitionController : Controller
     {
+
         [Route("NewReq")]
         public ActionResult NewReq()
         {
-            return View();
+            using (SSISdbEntities e = new SSISdbEntities()) {
+                string SRcount = (e.StaffRequisitionHeaders.Count()+1).ToString();
+                Session["currentFormId"]= "SR" + SRcount;
+
+                Tuple<Item, StaffRequisitionDetail> tuple = new Tuple<Item, StaffRequisitionDetail>(new Item(), new StaffRequisitionDetail());
+
+                //ViewBag.ItemsList = new SelectList(e.Items.ToList(), "ItemCode", "Description");
+                ViewBag.ItemsList = new SelectList((from s in e.Items.ToList()
+                                                    select new
+                                                    {
+                                                        ItemCode = s.ItemCode,
+                                                        Description = s.Description + " (" + s.UoM + ")"
+                                                    }),
+                                                    "ItemCode",
+                                                    "Description",
+                                                    null);
+
+                return View(tuple);
+            }
+
         }
 
         [Route("BackOrders")]
@@ -23,6 +43,94 @@ namespace SA46Team1_Web_ADProj.Controllers
             return View();
         }
 
-     
+        [HttpPost]
+        public RedirectToRouteResult DiscardSelBackorders(JsonResult toDeleteData, int noOfRecords)
+        {
+            Session["DeptReqTabIndex"] = "1";
+
+            //using (SSISdbEntities e = new SSISdbEntities())
+            //{
+            //    for(int i=0;i<noOfRecords;i++)
+            //    {
+            //        toDeleteData["ItemCode"]
+            //    }
+            //    StaffRequisitionDetail srd = new StaffRequisitionDetail();
+            //    //srd.CancelledBackOrdered = data;
+            //    DAL.StaffRequisitionDetailsRepositoryImpl dal = new DAL.StaffRequisitionDetailsRepositoryImpl(e);
+            //    dal.UpdateStaffRequisitionDetail(srd);
+
+            //    //foreach row returned, retrieve and update obj accordingly.
+            //    e.SaveChanges();
+            //}
+
+            return RedirectToAction("Requisition", "Dept");
+        }
+
+        [HttpPost]
+        [Route("NewReq/AddNewReqItem")]
+        public RedirectToRouteResult AddNewReqItem(Item item1, StaffRequisitionDetail item2)
+        {
+            Item itemToAdd = new Item();
+
+            using (SSISdbEntities e = new SSISdbEntities())
+            {
+                string itemCode = Request.Form["SelectItemDesc"].ToString();
+                itemToAdd= e.Items.Where(x => x.ItemCode == itemCode).FirstOrDefault();
+
+                List<StaffRequisitionDetail> list = new List<StaffRequisitionDetail>();
+                list = (List<StaffRequisitionDetail>)Session["newReqList"];
+                StaffRequisitionDetail srd = new StaffRequisitionDetail();
+                srd.ItemCode = itemToAdd.ItemCode;
+                srd.FormID = Session["currentFormId"].ToString();
+                srd.QuantityOrdered = item2.QuantityOrdered;
+                srd.QuantityDelivered = 0;
+                srd.QuantityBackOrdered = 0;
+                srd.CancelledBackOrdered = 0;
+
+                srd.Item = e.Items.Where(x => x.ItemCode == itemToAdd.ItemCode).FirstOrDefault();
+
+                srd.Item.Description = itemToAdd.Description;
+                srd.Item.UoM = itemToAdd.UoM;
+
+                list.Add(srd);
+                Session["newReqList"] = list;
+
+                return RedirectToAction("Requisition", "Dept");
+            }
+        }
+
+        [HttpPost]
+        //[Route("NewReq/SubmitNewRequestForm")]
+        public RedirectToRouteResult SubmitNewRequestForm()
+        {
+            using (SSISdbEntities e = new SSISdbEntities())
+            {
+                DAL.StaffRequisitionDetailsRepositoryImpl dalDetails = new DAL.StaffRequisitionDetailsRepositoryImpl(e);
+                DAL.StaffRequisitionRepositoryImpl dalHeader = new DAL.StaffRequisitionRepositoryImpl(e);
+
+                StaffRequisitionHeader srh = new StaffRequisitionHeader();
+                srh.FormID = Session["currentFormId"].ToString();
+                srh.DepartmentCode = Session["DepartmentCode"].ToString();
+                srh.EmployeeID = Session["UserId"].ToString();
+                srh.DateRequested = System.DateTime.Now;
+                srh.Status = "Pending"; //to check
+                srh.ApprovalStatus = "Pending";
+                srh.DateProcessed = System.DateTime.Now;
+                srh.Approver = e.Employees.Where(x => x.EmployeeID == srh.EmployeeID).Select(x => x.ReportsTo).FirstOrDefault();
+
+                dalHeader.InsertStaffRequisitionHeader(srh);
+
+                foreach (StaffRequisitionDetail srd in (List<StaffRequisitionDetail>)Session["newReqList"]) {
+                    srd.Item = null;
+                    dalDetails.InsertStaffRequisitionDetail(srd);
+                }
+
+                e.SaveChanges();
+                Session["newReqList"] = new List<Models.StaffRequisitionDetail>();
+            }
+
+            return RedirectToAction("Requisition", "Dept");
+        }
+
     }
 }
