c1113d8ab86b81a93254f3339e45c189bf363dab
SandyIsHere <sandyongsowt@gmail.com>
2018-07-20 14:25:38 +0800

    added skeleton for Dept Approver- notificcations

---
+++ SA46Team1_Web_ADProj/Controllers/DeptNotificationsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using SA46Team1_Web_ADProj.Models;
+
+namespace SA46Team1_Web_ADProj.Controllers
+{
+    [RoutePrefix("Dept/DeptNotifications")]
+    public class DeptNotificationsController : Controller
+    {
+        [Route("Read")]
+        public ActionResult Read()
+        {
+            return View("ReadMsgs");
+        }
+
+        [Route("Unread")]
+        public ActionResult Unread()
+        {
+            return View("UnreadMsgs");
+        }
+
+        [Route("All")]
+        public ActionResult All()
+        {
+            return View("AllMsgs");
+        }
+
+
+    }
+}
