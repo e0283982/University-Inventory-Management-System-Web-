91990ff1e1b0bc4930de9b47bfe5a914111c1bcb
SandyIsHere <sandyongsowt@gmail.com>
2018-07-25 18:14:27 +0800

    working Edit dept req history details

---
+++ SA46Team1_Web_ADProj/Models/ReqHistoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SA46Team1_Web_ADProj.Models
+{
+    public class ReqHistoryModel
+    {
+        public string ApprovalStatus { get; set; }
+
+        public DateTime RequestDate { get; set; }
+
+        public string RepName { get; set; }
+
+        public string ApproverName { get; set; }
+    }
+}
