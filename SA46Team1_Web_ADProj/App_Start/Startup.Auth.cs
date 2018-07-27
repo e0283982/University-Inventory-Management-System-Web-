374907716bd6551fcf75fea3a7295e147c830228
Jayden <wwj.jayden@gmail.com>
2018-07-27 11:10:19 +0800

    Remake PO details page from JSON data to controller data

--- SA46Team1_Web_ADProj/App_Start/Startup.Auth.cs
+++ SA46Team1_Web_ADProj/App_Start/Startup.Auth.cs
@@ -25,7 +25,7 @@ namespace SA46Team1_Web_ADProj
            {
                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                LoginPath = new PathString("/Main/login"),
-               ExpireTimeSpan = TimeSpan.FromMinutes(2)
+               ExpireTimeSpan = TimeSpan.FromMinutes(30)
            });
 
         }
@@ -35,7 +35,7 @@ namespace SA46Team1_Web_ADProj
             {
                 AllowInsecureHttp = true,
                 TokenEndpointPath = new PathString("/token"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(5),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
                 Provider = new SimpleAuthorizationServerProvider()
             };
             // Token Generation
