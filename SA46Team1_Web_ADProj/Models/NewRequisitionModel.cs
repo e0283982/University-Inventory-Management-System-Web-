350f077346c15f3aaa7f25460bc11a2509bf02a5
Hendri <hendrisetiawardana@gmail.com>
2018-07-28 21:30:38 +0800

    updated the staff requisition details for android
    updated the staff requisition details for android

--- SA46Team1_Web_ADProj/Models/NewRequisitionModel.cs
+++ SA46Team1_Web_ADProj/Models/NewRequisitionModel.cs
@@ -10,7 +10,9 @@ namespace SA46Team1_Web_ADProj.Models
 
         public string EmployeeId { get; set; }   
         public string ItemDescription { get; set; }
-        public int OrderedQuantity { get; set; }        
+        public int OrderedQuantity { get; set; }
+        public int RequisitionSize { get; set; }
+        public int RequisitionId { get; set; }
 
     }
 }
