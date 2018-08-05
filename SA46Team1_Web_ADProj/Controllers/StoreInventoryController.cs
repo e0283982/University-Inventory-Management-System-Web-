2570176a39a46b0c89b1f65c9b2a9bb9674ec45e
Jayden <wwj.jayden@gmail.com>
2018-08-05 12:28:11 +0800

    stocktake validation

--- SA46Team1_Web_ADProj/Controllers/StoreInventoryController.cs
+++ SA46Team1_Web_ADProj/Controllers/StoreInventoryController.cs
@@ -528,68 +528,79 @@ namespace SA46Team1_Web_ADProj.Controllers
         [HttpPost]
         public ActionResult StockTakeUpdate(StockTakeList[] arr, string[] arr1)
         {
-            int count = 0;
-
-            string transType = "Stock Take";
-            List<StockTakeList> list = new List<StockTakeList>();
-            for (int i = 0; i < arr.Length; i++)
+            try
             {
-                list.Add(arr[i]);
-            }
+                int count = 0;
 
-            using (SSISdbEntities m = new SSISdbEntities())
-            {
-                int itemTransCount = m.StockTakeHeaders.Count() + 1;
-                string itemTransactionId = CommonLogic.SerialNo(itemTransCount, "ST");
-
-                // Update StockTakeHeader Table
-                StockTakeHeader stockTakeHeader = new StockTakeHeader();
-                stockTakeHeader.StockTakeID = itemTransactionId;
-                stockTakeHeader.Date = DateTime.Now;
-                stockTakeHeader.TransactionType = transType;
-                m.StockTakeHeaders.Add(stockTakeHeader);
-                m.SaveChanges();
-
-                foreach (StockTakeList l in list)
+                string transType = "Stock Take";
+                List<StockTakeList> list = new List<StockTakeList>();
+                for (int i = 0; i < arr.Length; i++)
                 {
-                    Item item = m.Items.Where(x => x.ItemCode == l.ItemCode).FirstOrDefault();
-                    int itemQty = Convert.ToInt32(arr1[count]);
-                    float avgCost = item.AvgUnitCost;
-                    int qtyOnHand = item.Quantity;
-                    int qtyAdjusted = itemQty - qtyOnHand;
-                    float totalAmt = avgCost * (float)qtyAdjusted;
-                    string itemcode = l.ItemCode;
+                    list.Add(arr[i]);
+                }
 
-                    if (qtyAdjusted != 0)
-                    {
-                        // Update Item Table
-                        item.Quantity = itemQty;
+                using (SSISdbEntities m = new SSISdbEntities())
+                {
+                    int itemTransCount = m.StockTakeHeaders.Count() + 1;
+                    string itemTransactionId = CommonLogic.SerialNo(itemTransCount, "ST");
 
-                        // Update ItemTransaction Table
-                        ItemTransaction itemTransaction = new ItemTransaction();
-                        itemTransaction.TransDateTime = DateTime.Now;
-                        itemTransaction.DocumentRefNo = itemTransactionId;
-                        itemTransaction.ItemCode = itemcode;
-                        itemTransaction.TransactionType = transType;
-                        itemTransaction.Quantity = qtyAdjusted;
-                        itemTransaction.UnitCost = avgCost;
-                        itemTransaction.Amount = totalAmt;
-                        m.ItemTransactions.Add(itemTransaction);
-                        m.SaveChanges();
-                    }
-
-                    // Update StockTakeDetails Table
-                    StockTakeDetail stockTakeDetail = new StockTakeDetail();
-                    stockTakeDetail.StockTakeID = itemTransactionId;
-                    stockTakeDetail.ItemCode = itemcode;
-                    stockTakeDetail.QuantityOnHand = qtyOnHand;
-                    stockTakeDetail.QuantityCounted = itemQty;
-                    stockTakeDetail.QuantityAdjusted = qtyAdjusted;
-                    m.StockTakeDetails.Add(stockTakeDetail);
+                    // Update StockTakeHeader Table
+                    StockTakeHeader stockTakeHeader = new StockTakeHeader();
+                    stockTakeHeader.StockTakeID = itemTransactionId;
+                    stockTakeHeader.Date = DateTime.Now;
+                    stockTakeHeader.TransactionType = transType;
+                    m.StockTakeHeaders.Add(stockTakeHeader);
                     m.SaveChanges();
-                    count++;
+
+                    foreach (StockTakeList l in list)
+                    {
+                        Item item = m.Items.Where(x => x.ItemCode == l.ItemCode).FirstOrDefault();
+                        int itemQty = Convert.ToInt32(arr1[count]);
+                        float avgCost = item.AvgUnitCost;
+                        int qtyOnHand = item.Quantity;
+                        int qtyAdjusted = itemQty - qtyOnHand;
+                        float totalAmt = avgCost * (float)qtyAdjusted;
+                        string itemcode = l.ItemCode;
+
+                        if (qtyAdjusted != 0)
+                        {
+                            // Update Item Table
+                            item.Quantity = itemQty;
+
+                            // Update ItemTransaction Table
+                            ItemTransaction itemTransaction = new ItemTransaction();
+                            itemTransaction.TransDateTime = DateTime.Now;
+                            itemTransaction.DocumentRefNo = itemTransactionId;
+                            itemTransaction.ItemCode = itemcode;
+                            itemTransaction.TransactionType = transType;
+                            itemTransaction.Quantity = qtyAdjusted;
+                            itemTransaction.UnitCost = avgCost;
+                            itemTransaction.Amount = totalAmt;
+                            m.ItemTransactions.Add(itemTransaction);
+                            m.SaveChanges();
+                        }
+
+                        // Update StockTakeDetails Table
+                        StockTakeDetail stockTakeDetail = new StockTakeDetail();
+                        stockTakeDetail.StockTakeID = itemTransactionId;
+                        stockTakeDetail.ItemCode = itemcode;
+                        stockTakeDetail.QuantityOnHand = qtyOnHand;
+                        stockTakeDetail.QuantityCounted = itemQty;
+                        stockTakeDetail.QuantityAdjusted = qtyAdjusted;
+                        m.StockTakeDetails.Add(stockTakeDetail);
+                        m.SaveChanges();
+                        count++;
+                    }
                 }
             }
+            catch(FormatException fe)
+            {
+                Debug.WriteLine(fe.Message);
+            }catch(Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            
             return View();
         }
 
