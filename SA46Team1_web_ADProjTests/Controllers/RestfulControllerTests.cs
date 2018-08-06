ca9e9a5b302e5445b272c5abe009f3cffca50485
SandyIsHere <sandyongsowt@gmail.com>
2018-08-07 03:36:41 +0800

    added test folder

---
+++ SA46Team1_web_ADProjTests/Controllers/RestfulControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SA46Team1_Web_ADProj.Controllers;
+using SA46Team1_Web_ADProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA46Team1_Web_ADProj.Controllers.Tests
+{
+    [TestClass()]
+    public class RestfulControllerTests
+    {
+        [TestMethod()]
+        public void GetShortItemListTest()
+        {
+            //using (SSISdbEntities e = new SSISdbEntities())
+            //{
+            //    e.Configuration.ProxyCreationEnabled = false;
+            //}
+
+            var controller = new RestfulController();
+            var result = controller.GetShortItemList();
+            Assert.IsNotNull(result);
+        }
+    }
+}
